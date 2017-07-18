2e619b73d43cce4d5635054057ff93eb2f4c7cfb
Smolyanov Igor <smolyan_91@mail.ru>
2017-07-18 22:17:35 +0300

    Create model

---
+++ Splitter/src/main/java/splitter/model/Splitle.java
@@ -0,0 +1,73 @@
+package splitter.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Splitle")
+public class Splitle {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int ID;
+
+    @Column(name = "MESSAGE")
+    private String MESSAGE;
+
+    @Column(name = "DATE")
+    private Date DATE;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getMESSAGE() {
+        return MESSAGE;
+    }
+
+    public void setMESSAGE(String MESSAGE) {
+        this.MESSAGE = MESSAGE;
+    }
+
+    public Date getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(Date DATE) {
+        this.DATE = DATE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Splitle splitle = (Splitle) o;
+
+        if (ID != splitle.ID) return false;
+        if (MESSAGE != null ? !MESSAGE.equals(splitle.MESSAGE) : splitle.MESSAGE != null) return false;
+        return DATE != null ? DATE.equals(splitle.DATE) : splitle.DATE == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ID;
+        result = 31 * result + (MESSAGE != null ? MESSAGE.hashCode() : 0);
+        result = 31 * result + (DATE != null ? DATE.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Splitle{" +
+                "ID=" + ID +
+                ", MESSAGE='" + MESSAGE + '\'' +
+                ", DATE=" + DATE +
+                '}';
+    }
+}
