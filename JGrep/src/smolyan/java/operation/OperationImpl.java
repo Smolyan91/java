5f3f37211b04e2ca5a008496b28c8bd685de7007
Smolyanov Igor <smolyan_91@mail.ru>
2017-07-23 02:00:15 +0300

    Create search feature

--- JGrep/src/smolyan/java/operation/OperationImpl.java
+++ JGrep/src/smolyan/java/operation/OperationImpl.java
@@ -3,11 +3,20 @@ package smolyan.java.operation;
 import smolyan.java.DataArguments;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class OperationImpl implements Operation{
 
     private DataArguments dataArguments;
+    private List<Path> listMessageSuccess;
+    private List<String> listMessageError;
 
     public OperationImpl(DataArguments dataArguments) {
         this.dataArguments = dataArguments;
@@ -17,10 +26,11 @@ public class OperationImpl implements Operation{
             throws IOException{
         if (this.dataArguments != null){
             switch (dataArguments.getCommandDA()){
-                case GREP: this.grep(this.dataArguments.getMassArgs()[1], this.dataArguments.getMassArgs()[2]);break;
-                case CP  : this.copy(this.dataArguments.getMassArgs()[1], this.dataArguments.getMassArgs()[2]);break;
-                case MV  : this.move(this.dataArguments.getMassArgs()[1], this.dataArguments.getMassArgs()[2]);break;
-                case RM  : this.remove(this.dataArguments.getMassArgs()[1]);break;
+                case GREP: this.grep(this.dataArguments.getMassArgs()[0], this.dataArguments.getMassArgs()[1]);break;
+                case CP  : this.copy(this.dataArguments.getMassArgs()[0], this.dataArguments.getMassArgs()[1]);break;
+                case MV  : this.move(this.dataArguments.getMassArgs()[0], this.dataArguments.getMassArgs()[1]);break;
+                case SRCH: this.search(this.dataArguments.getMassArgs()[0], this.dataArguments.getMassArgs()[1]);break;
+                case RM  : this.remove(this.dataArguments.getMassArgs()[0]);break;
                 default:break;
             }
         }else System.out.println("DataArguments is null!!!");//TODO исследовать
@@ -28,21 +38,45 @@ public class OperationImpl implements Operation{
 
     @Override
     public void grep(String pattern, String pathToFile) throws IOException {
+        Path path = Paths.get(pathToFile).toRealPath();
+
 
     }
 
     @Override
     public void remove(String pathToFile) throws IOException {
+        Path path = Paths.get(pathToFile).toRealPath();
 
     }
 
     @Override
     public void move(String locatePathFile, String newPathFile) throws IOException {
+        Path from = Paths.get(locatePathFile).toRealPath();
+        Path to = Paths.get(newPathFile).toRealPath();
 
     }
 
     @Override
     public void copy(String originalFile, String targetFile) throws IOException {
+        Path from = Paths.get(originalFile).toRealPath();
+        Path to = Paths.get(targetFile).toRealPath();
 
+    }
+
+    @Override
+    public void search(String pattern, String filePath)throws IOException{
+        Path path = Paths.get(filePath).toRealPath();
+        try (DirectoryStream<Path>directoryStream = Files.newDirectoryStream(path, "*" + pattern + "*")){
+            if (directoryStream != null){
+                listMessageSuccess = new LinkedList<>();
+                for (Path entry: directoryStream){
+                    listMessageSuccess.add(entry.getFileName());
+                    System.out.println(entry.getFileName());
+                }
+            }
+        }catch (Exception e){
+            listMessageError = new ArrayList<>();
+            listMessageError.add(e.getMessage());
+        }
     }
 }
