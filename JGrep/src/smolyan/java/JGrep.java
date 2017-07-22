cf4e4daf773a271f75e8ec0e3f18ad99a6cd7cac
Smolyanov Igor <smolyan_91@mail.ru>
2017-07-23 01:06:20 +0300

    Create art

--- JGrep/src/smolyan/java/JGrep.java
+++ JGrep/src/smolyan/java/JGrep.java
@@ -2,43 +2,83 @@ package smolyan.java;
 
 
 import smolyan.java.commands.Operation;
+import smolyan.java.operation.OperationImpl;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class JGrep {
 
-    private static Path PATH_TO_FILE;
-    private static Operation OPERATION;
-    private static String PATTERN_TO_SEARCH;
-
-
     public static void main(String[] args) throws IOException {
         //TODO выводить сообщения
-        if (args.length == 0 || args.length < 3) { System.out.println("Incorrect request'"); return;}
-        OPERATION = initTypeCommand(args[0]); // detect type operation
-        PATTERN_TO_SEARCH = args[1].trim();
-        PATH_TO_FILE = Paths.get(args[2]).toRealPath();
+        if (args.length == 0) { System.out.println("Incorrect request'"); return;}
+        new OperationImpl(initTypeCommand(args)).start();
     }
 
-    private static Operation initTypeCommand(String typeCommand){
+    private static DataArguments initTypeCommand(String[] arrayArgs){
         Operation operation;
+        DataArguments da;
+        String typeCommand = arrayArgs[0];
+        int countArgs = arrayArgs.length;
+        String[] arr = Arrays.copyOfRange(arrayArgs, 1, countArgs);
         switch (typeCommand.toLowerCase()){
-            case "grep" : operation = Operation.GREP; break;
-            case "del"  : operation = Operation.RM  ; break;
-            case "rm"   : operation = Operation.RM  ; break;
-            case "cp"   : operation = Operation.CP  ; break;
-            case "mv"   : operation = Operation.MV  ; break;
-            case "crd"  : operation = Operation.CRD ; break;
-            case "crf"  : operation = Operation.CRF ; break;
-            case "help" : operation = Operation.HELP; break;
+            case "grep" : {
+                if (countArgs == 3){
+                    operation = Operation.GREP;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "del"  : {
+                if(countArgs == 2){
+                    operation = Operation.RM;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "rm"   : {
+                if(countArgs == 2){
+                    operation = Operation.RM  ;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "cp"   : {
+                if(countArgs == 3){
+                    operation = Operation.CP  ;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "mv"   : {
+                if(countArgs == 3){
+                    operation = Operation.MV  ;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "crd"  : {
+                //Program can create a maximum of 5 folders
+                if(countArgs <= 5){
+                    operation = Operation.CRD ;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "crf"  : {
+                //Program can create a maximum of 10 files
+                if(countArgs <= 10){
+                    operation = Operation.CRF;
+                }else operation = Operation.ERROR;
+            } break;
+
+            case "help" : {
+                if (countArgs == 2){
+                    operation = Operation.HELP;
+                } else operation = Operation.ERROR;
+            } break;
+
             default: //TODO перенести в менеджер сообщений
                 operation = Operation.ERROR;
                 System.out.println("Inter correct command. You can write in command line <jcom help> then you can " +
                         "see information about this commands");
                 break;
         }
-        return operation;
+        da = new DataArguments(operation, countArgs, arr);
+        return da;
     }
 }
---
+++ JGrep/src/smolyan/java/operation/Operation.java
@@ -0,0 +1,11 @@
+package smolyan.java.operation;
+
+
+import java.io.IOException;
+
+public interface Operation {
+    void grep(String pattern, String pathToFile) throws IOException;
+    void remove(String pathToFile) throws IOException;
+    void move(String locatePathFile, String newPathFile) throws IOException;
+    void copy(String originalFile, String targetFile) throws IOException;
+}
